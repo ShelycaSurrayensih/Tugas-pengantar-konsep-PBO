04917923e9147155560020ebad8142fd3467b95a
Shelyca Surrayensih <70507995+ShelycaSurrayensih@users.noreply.github.com>
2020-09-27 20:22:31 +0700

    Delete MainPertanyaan.java

--- praktikum/Shelyca/relasiclass/percobaan3/MainPertanyaan.java
+++
@@ -1,9 +0,0 @@
-package Shelyca.relasiclass.percobaan3;
-public class MainPertanyaan {
-       public static void main(String[] args) {
-        Pegawai masinis = new Pegawai("1234", "Spongebob Squarepants");
-        KeretaApi keretaApi = new KeretaApi("Gaya Baru", "Bisnis", masinis);
-        
-         System.out.println(keretaApi.info());
-    }
-}
