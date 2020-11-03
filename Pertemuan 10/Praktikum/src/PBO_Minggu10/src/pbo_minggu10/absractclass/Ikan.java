9109948c8417c993882d29651ca0292de21b2bf7
Shelyca Surrayensih <70507995+ShelycaSurrayensih@users.noreply.github.com>
2020-11-03 18:53:32 +0700

    Add files via upload

---
+++ Pertemuan 10/Praktikum/src/PBO_Minggu10/src/pbo_minggu10/absractclass/Ikan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_minggu10.absractclass;
+
+/**
+ *
+ * @author WIN10
+ */
+public class Ikan  extends Hewan{
+    public abstract void bergerak();
+//    @Override
+//    public void bergerak(){
+//        System.out.println("Berenang dengan sirip, \"wush..wush..\"");
+//    }
+    
+}
